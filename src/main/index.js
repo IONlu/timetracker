34fd7f88a5706ccbab2a6a7a0ff76cb3e0541ed2
Daniel Duton <daniel@duton.lu>
2018-02-20 08:00:42 +0100

    set initial window size

--- src/main/index.js
+++ src/main/index.js
@@ -20,9 +20,9 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    height: 800,
     useContentSize: true,
-    width: 1000
+    width: process.env.NODE_ENV === 'development' ? 1000 : 450
   })
 
   mainWindow.loadURL(winURL)
